4034592759d5d087e6d138a5829f28665a0f8d34
slipkinem <398873901@qq.com>
2017-04-01 18:26:59 +0800

    init repo

--- src/main/java/cn/lvsen/test/controller/UserController.java
+++ src/main/java/cn/lvsen/test/controller/UserController.java
@@ -1,8 +1,12 @@
 package cn.lvsen.test.controller;
 
+import cn.lvsen.test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by slipkinem on 2017/4/1.
  */
 public class UserController {
-
+    @Autowired
+    private UserService userService;
 }
